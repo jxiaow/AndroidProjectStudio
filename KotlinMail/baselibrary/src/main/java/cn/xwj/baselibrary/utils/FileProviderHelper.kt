17e4237d3ed416552287fdac917949989d3588cb
xiaowujiang <xiaowu_zhu@126.com>
2018-05-30 23:05:29 +0800

    update

--- KotlinMail/baselibrary/src/main/java/cn/xwj/baselibrary/utils/FileProviderHelper.kt
+++ KotlinMail/baselibrary/src/main/java/cn/xwj/baselibrary/utils/FileProviderHelper.kt
@@ -11,33 +11,32 @@ import java.io.File
  * Date: 2018-05-30 15:04:13
  * Description: FileProviderHelper: .
  */
-class FileProviderHelper {
+object FileProviderHelper {
 
-    companion object {
-        fun fromFileUri(context: Context, file: File): Uri {
-            support23 {
-                return FileProvider.getUriForFile(context,
-                        "${context.packageName}.fileProvider",
-                        file)
-            }
-            return Uri.fromFile(file)
+    fun fromFileUri(context: Context, file: File): Uri {
+        support23 {
+            return FileProvider.getUriForFile(context,
+                    "${context.packageName}.fileProvider",
+                    file)
         }
+        return Uri.fromFile(file)
+    }
 
 
-        fun setIntentTypeAndData(context: Context, intent: Intent, type: String,
-                                 file: File, isWriteable: Boolean) {
-            intent.setDataAndType(fromFileUri(context, file), type)
-            support23 {
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                if (isWriteable) intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-            }
+    fun setIntentTypeAndData(context: Context, intent: Intent, type: String,
+                             file: File, isWriteable: Boolean) {
+        intent.setDataAndType(fromFileUri(context, file), type)
+        support23 {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            if (isWriteable) intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
         }
+    }
 
+    private inline fun support23(code: () -> Unit) {
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            code()
+        }
     }
 }
 
-private inline fun support23(code: () -> Unit) {
-    if (android.os.Build.VERSION.SDK_INT > 23) {
-        code()
-    }
-}
+
