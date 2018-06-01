f1043729abcf1baa341baa7c34553eff1e1ecd47
xiaowujiang <i.xiaowujiang@gmail.com>
2018-06-01 17:36:45 +0800

    update

--- KotlinMail/app/src/main/java/cn/xwj/kotlinmail/ui/fragment/HomeFragment.kt
+++ KotlinMail/app/src/main/java/cn/xwj/kotlinmail/ui/fragment/HomeFragment.kt
@@ -6,6 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import cn.xwj.baselibrary.ui.fragment.BaseFragment
 import cn.xwj.kotlinmail.R
+import cn.xwj.kotlinmail.ui.utils.BannerImageLoader
+import com.kotlin.mall.common.MainConstant.Companion.HOME_BANNER_FOUR
+import com.kotlin.mall.common.MainConstant.Companion.HOME_BANNER_ONE
+import com.kotlin.mall.common.MainConstant.Companion.HOME_BANNER_THREE
+import com.kotlin.mall.common.MainConstant.Companion.HOME_BANNER_TWO
+import com.youth.banner.BannerConfig
+import com.youth.banner.Transformer
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * Author: xw
@@ -16,5 +24,41 @@ class HomeFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initBanner()
+        initNews()
+    }
+
+    private fun initNews() {
+        mNewsFlipperView.setData(arrayOf("夏日炎炎，第一波福利还有30秒到达战场", "新用户立领1000元优惠券"))
+    }
+
+    private fun initBanner() {
+
+        mHomeBanner.setImageLoader(BannerImageLoader())
+        mHomeBanner.setImages(listOf(HOME_BANNER_ONE, HOME_BANNER_TWO, HOME_BANNER_THREE, HOME_BANNER_FOUR))
+        mHomeBanner.setBannerAnimation(Transformer.Accordion)
+        mHomeBanner.setDelayTime(2000)
+        mHomeBanner.setIndicatorGravity(BannerConfig.RIGHT)
+        mHomeBanner.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mHomeBanner.startAutoPlay()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mHomeBanner.stopAutoPlay()
+    }
+
+
+    private fun initView() {
+
     }
 }
---
+++ KotlinMail/app/src/main/java/cn/xwj/kotlinmail/ui/utils/BannerImageLoader.kt
@@ -0,0 +1,19 @@
+package cn.xwj.kotlinmail.ui.utils
+
+import android.content.Context
+import android.widget.ImageView
+import cn.xwj.baselibrary.ext.loadUrl
+import com.youth.banner.loader.ImageLoader
+
+/**
+ * Author: xw
+ * Date: 2018-06-01 17:23:31
+ * Description: BannerImageLoader: .
+ */
+class BannerImageLoader : ImageLoader() {
+
+    override fun displayImage(context: Context?, path: Any?, imageView: ImageView?) {
+        imageView?.loadUrl(path.toString())
+    }
+
+}
